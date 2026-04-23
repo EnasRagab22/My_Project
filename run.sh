939b53485534ea987a7905dc2a21020eeaf386c9
EnasRagab <enasragab226@gmail.com>
2026-04-23 11:24:21 +0200

    edit copy file

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 sort $1 | uniq -c | awk -F' ' '{print $2 " --> "$1}' > group.txt
 cat group.txt
-cp group.txt C:\Users\Microsoft\Desktop\My_Project\My_Project
+cp group.txt c/Users/Microsoft/Desktop/My_Project/My_Project
